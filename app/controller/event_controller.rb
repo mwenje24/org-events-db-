f7a819781a7dd1f733bf6a37eacb830487a2724f
Mwenje <mwenje24@gmail.com>
2022-09-09 05:53:04 +0300

    added a post request to the event controller

--- app/controller/event_controller.rb
+++ app/controller/event_controller.rb
@@ -3,4 +3,19 @@ class EventController < Sinatra::Base
         events = Event.all.order(:date)
         events.to_json
     end
+
+    post '/events' do
+        event = Event.create(
+          title: params[:title],
+          category: params[:category],
+          location: params[:location],
+          time: params[:time],
+          event_owner_id: params[:event_owner_id],
+          description: params[:description],
+          fee: params[:fee],
+          date: params[:date],
+          image: params[:image]
+        )
+        event.to_json
+    end
 end
